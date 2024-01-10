534ac27c6098bf17714dfaf6413fdcb94e67920e
SOURAV KUMAR PANDA <74825918+sourav123608@users.noreply.github.com>
2024-01-10 08:12:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./component/Header";
+import Home from "./component/Home";
+import Coins from "./component/Coins";
+import Exchanges from "./component/Exchanges";
+import CoinDetails from "./component/CoinDetails";
+import Footer from "./component/Footer";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/coin:id" element={<CoinDetails />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
